17d27225794001619ac1bfef2b3ba32da3f81a37
Mashima <52222646+zimashima@users.noreply.github.com>
2019-11-07 15:50:25 -0500

    installed reactstrap and style component

---
+++ src/Components/HeaderFooter.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>NASA Photo of the day</h1>
+        </header>
+    )
+}
+
+export const Footer = () => {
+    return (
+        <footer>
+            <p>Disclaimer: These photos are pulled from NASA's API.</p>
+        </footer>
+    )
+}
